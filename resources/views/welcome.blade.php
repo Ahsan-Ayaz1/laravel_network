ff199201d69c73bba84a4f0d97748e357a50ddf9
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2025-01-15 22:06:03 +0500

    feat: :tada: Begin a New Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Network</title>
+</head>
+<body>
+    <h1>Welcome to the Laravel Network</h1>
+    <p>Click the button below to view the list</p>
+    <a href="#" class="btn">
+        Find Laravel Network
+    </a>
+</body>
+</html>
