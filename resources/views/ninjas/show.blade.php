0d8c3742819e1da1eabf2b2caf95bbb746b71894
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2025-01-15 22:46:47 +0500

    feat: :sparkles: Implement Route WildCard

---
+++ resources/views/ninjas/show.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninjas Network | Show</title>
+</head>
+<body>
+    <h1>Ninjas Detail</h1>
+    <p>{{ $id }}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route:: get('/ninjas', function() {
-    return view('ninjas.index');
+Route::get('/ninjas', function() {
+    $ninjas = [
+        [ "name" => "Mario", "skills" => 75, "id" => 1 ],
+        [ "name" => "Josh", "skills" => 45, "id" => 2 ],
+    ];
+    return view('ninjas.index', ["ninjas" => $ninjas]);
+});
+Route::get("/ninjas/{id}", function($id) {
+    return view("ninjas.show", [ "id" => $id ]);
 });
