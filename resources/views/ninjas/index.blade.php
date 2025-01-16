fd733aecd73e36282ff0928be9fb8518624a85ff
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2025-01-16 23:08:25 +0500

    feat: :sparkles: Creaate Layout Folder

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Currently Available Ninjas</h2>
     <ul>
-        @foreach ($ninjas as $ninja )
+        @foreach ($ninjas as $ninja)
             <li>
                 <a href="ninjas/{{ $ninja["id"] }}"> {{ $ninja["name"] }} </a>
             </li>
