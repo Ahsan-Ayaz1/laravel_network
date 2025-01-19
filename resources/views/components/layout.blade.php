a4ac654b4a91cbebf8fb119d2796efb01c71486a
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2025-01-19 21:36:51 +0500

    feat: :sparkles: Create Global Layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,1 +1,21 @@
 {{-- This is Global Layout we will use --}}
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Network</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>Ninja Network</h1>
+            <a href="/ninjas">All Ninjas</a>
+            <a href="/ninjas/create">Create New Ninja</a>
+            <main class="container">
+                {{ $slot }}
+            </main>
+        </nav>
+    </header>
+</body>
+</html>
--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,18 +1,10 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ninjas Network | Home</title>
-</head>
-<body>
+<x-layout>
     <h2>Currently Available Ninjas</h2>
     <ul>
         @foreach ($ninjas as $ninja)
-            <li>
-                <a href="ninjas/{{ $ninja["id"] }}"> {{ $ninja["name"] }} </a>
-            </li>
+        <li>
+            <a href="ninjas/{{ $ninja[" id"] }}"> {{ $ninja["name"] }} </a>
+        </li>
         @endforeach
     </ul>
-</body>
-</html>
+</x-layout>
